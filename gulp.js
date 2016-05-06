8c1532e91c5ad945807e87daa0232d0630253817
chenliexin <chenliexin@yeah.net>
2016-05-06 15:16:53 +0800

    201505061516

---
+++ gulp.js
@@ -0,0 +1,61 @@
+// api
+
+// gulp.src(globs[, options])
+// 这个是路由？
+
+// gulp.dest(path[, options])
+// 这个是输出？
+
+// gulp.task(name[, deps], fn)
+// 这个是init？
+
+// gulp.watch(glob[, opts], tasks)
+// gulp.watch(glob[, opts, cb])
+// 监视
+
+/*
+你仅仅需要知道的5个gulp命令
+
+gulp.task(name, fn)这个你应经见过了
+
+gulp.run(tasks...)尽可能多的并行运行多个task
+
+gulp.watch(glob, fn)当glob内容发生改变时，执行fn
+
+gulp.src(glob)返回一个可读的stream
+
+gulp.dest(glob)返回一个可写的stream
+
+
+
+https://segmentfault.com/a/1190000003098076
+
+
+*/
+
+
+/*
+ * plugins
+ * https://www.npmjs.com/
+ * http://gulpjs.com/plugins/
+ * 
+*/
+
+// sass
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var sourcemaps = require('gulp-sourcemaps');
+gulp.task('sass', function() {
+    sass('src/sass/*.scss', {
+            sourcemap: true
+        })
+        .on('error', sass.logError)
+        .pipe(sourcemaps.write('', {
+            includeContent: false,
+            sourceRoot: 'source'
+        }))
+        .pipe(gulp.dest('src/css'));
+});
+gulp.task('sass:watch', function() {
+    gulp.watch('src/sass/*.scss', ['sass']);
+});
---
+++ markdown.md
@@ -0,0 +1,65 @@
+
+**strong**
+
+*italic*
+
+~~line~~
+
+++under++
+
+==mark==
+
+# h1
+## h2
+### h3
+#### h4
+##### h5
+###### h6
+
+
+---
+
+> quote quote
+
+- list1
+- list2
+    - sublist
+    - sublist
+
+1. list
+2. list
+3. sfsdf
+
+---
+
+    sfsdf
+        sfdsdlfj
+            sfsdf
+            .css { backgroud: #fff;}
+
+
+- [ ] asf
+- [ ] sfsdf
+- [x] sfdsdf
+
+[link](http://note.youdao.com/)
+
+![image](http://note.youdao.com/favicon.ico)
+
+
+```
+var js = 123;
+<div></div>
+    .css { backgroud: #fff;}
+```
+
+
+header 1 | header 2
+---|---
+row 1 col 1 | row 1 col 2
+row 2 col 1 | row 2 col 2
+
+        ver sdf;
+        
+    var js=234;
+    
