27210a3d3af14c6eea4c862d9de3c25f7056433f
liexin.chen <liexin.chen@talebase.com>
2017-09-26 14:42:29 +0800

    ==

--- app.js
+++ app.js
@@ -2,47 +2,12 @@ const Koa = require('koa');
 const koaStatic = require('koa-static');
 const app = new Koa();
 
-const babel = require('babel-core');
-
-console.log(babel.transform('1'));
-
-app.name = 'codelib';
-app.env = 'dev';
-
-// x-response-time
-
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  console.log(1);
-  await next();
-  const ms = Date.now() - start;
-  ctx.set('X-Response-Time', `${ms}ms`);
-  console.log(ctx.body);
-});
-
-app.use(koaStatic('img'));
-
-// logger
-
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  console.log(3);
-  await next();
-  const ms = Date.now() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
-  console.log(4);
-  return ctx.body = 'Hello World11';
-});
-
-// response
+app.use(koaStatic('dist'));
 
 app.use(async ctx => {
-  console.log(5);
   ctx.body = 'Hello World';
-  // ctx.state = 200;
-  // ctx.res.end()
-  console.log(6);
-
 });
 
-app.listen(3000);
+console.log('localhost:3388');
+
+app.listen(3388);
---
+++ code/webgl.md
@@ -0,0 +1,5 @@
+WebGL（全写Web Graphics Library）是一种3D绘图协议，这种绘图技术标准允许把JavaScript和OpenGL ES 2.0结合在一起，通过增加OpenGL ES 2.0的一个JavaScript绑定，WebGL可以为HTML5 Canvas提供硬件3D加速渲染，这样Web开发人员就可以借助系统显卡来在浏览器里更流畅地展示3D场景和模型了，还能创建复杂的导航和数据视觉化。
+
+OpenGL（全写Open Graphics Library）是指定义了一个跨编程语言、跨平台的编程接口规格的专业的图形程序接口。它用于三维图像（二维的亦可），是一个功能强大，调用方便的底层图形库。
+
+极强的编码能力
